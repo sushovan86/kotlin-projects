eb2abb38bd15853e1a465c541f6b45bc5f190f15
Sushovan <sushovan86@gmail.com>
2021-03-11 20:59:11 +0530

    Initial Commit

--- spring-microservice/config-server/build.gradle.kts
+++ spring-microservice/config-server/build.gradle.kts
@@ -1,17 +1,5 @@
-val springCloudVersion by extra("2020.0.1")
-
-repositories {
-    mavenCentral()
-    jcenter()
-}
+val springCloudVersion:String by extra("2020.0.1")
 
 dependencies {
     implementation("org.springframework.cloud:spring-cloud-config-server")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
-
-dependencyManagement {
-    imports {
-        mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
-    }
 }
